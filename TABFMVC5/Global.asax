0dee075caf830fdfadf8c9f40fd849bc372bc11e
Will <doggy.huang@gmail.com>
2016-09-06 15:35:32 +0800

    加入專案檔案。

---
+++ TABFMVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TABFMVC5.MvcApplication" Language="C#" %>
---
+++ TABFMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TABFMVC5.Startup))]
+namespace TABFMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
