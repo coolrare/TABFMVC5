acac197ddde3860e3eef63b1bcb0082ffa1d240d
Will <doggy.huang@gmail.com>
2016-09-06 17:56:47 +0800

    06 新增一個 /Home/Test 頁面

--- TABFMVC5/Controllers/HomeController.cs
+++ TABFMVC5/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TABFMVC5.Models;
 
 namespace TABFMVC5.Controllers
 {
@@ -23,7 +24,12 @@ namespace TABFMVC5.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+            
+            return View();
+        }
 
+        public ActionResult Test()
+        {
             return View();
         }
     }
