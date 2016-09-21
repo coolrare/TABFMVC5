df64f451debafdad62774326a20291cb5c3c2a21
Will <doggy.huang@gmail.com>
2016-09-21 15:56:15 +0800

    39 練習簡單模型繫結與複雜模型繫結與多筆複雜模型繫結

--- TABFMVC5/Controllers/MBController.cs
+++ TABFMVC5/Controllers/MBController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TABFMVC5.ViewModels;
 
 namespace TABFMVC5.Controllers
 {
@@ -26,6 +27,38 @@ namespace TABFMVC5.Controllers
             TempData["Error"] = "發生錯誤";
 
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Simple1(int id, string name)
+        {
+            return Content(id + " - " + name);
+        }
+
+        public ActionResult Simple1(FormCollection form)
+        {
+            return Content(form["id"] + " - " + form["name"]);
+        }
+
+        public ActionResult Complex1()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Complex1(UserLoginViewModel data)
+        {
+            return Content(data.username + " - " + data.password);
+        }
+
+        public ActionResult Complex2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Complex2(UserLoginViewModel data1, UserLoginViewModel data2)
+        {
+            return Content(data1.username + " - " + data1.password + " | " + data2.username + " - " + data2.password);
         }
     }
 }
