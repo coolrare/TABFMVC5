81de166926217f46ff4f2c7a5f13241da80a3ab9
Will <doggy.huang@gmail.com>
2016-09-26 17:56:43 +0800

    47 練習 Razor 語法

---
+++ TABFMVC5/Controllers/ViewTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TABFMVC5.Controllers
+{
+    public class ViewTestController : Controller
+    {
+        // GET: ViewTest
+        public ActionResult Index()
+        {
+            return PartialView();
+        }
+    }
+}
